285067636604dc928c1b72ef1310d0811549bf5b
JulianHinsch <jhinsch799@gmail.com>
2018-09-10 13:27:57 -0400

    restyle

--- client/src/components/PollList.js
+++ client/src/components/PollList.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import { Link } from 'react-router-dom';
 
 export default class PollList extends Component {
@@ -15,26 +16,54 @@ export default class PollList extends Component {
 	render() {
 		return (
 			<div className="content poll-list">
-				<h1>Polls</h1>
+				<h1>All Polls</h1>
+                <div className='link-container'>
+                    <div className='back'>
+                        <Link to="/">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg" 
+                                width="24" 
+                                height="24" 
+                                viewBox="0 0 24 24" 
+                                fill="none" 
+                                stroke="currentColor" 
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round">
+                                <polyline points="15 18 9 12 15 6"></polyline>
+                            </svg>
+                            Home
+                        </Link>
+                    </div>
+                    <div className='add-a-poll'>
+                        <Link to="/new">
+                            Add a Poll
+                            <svg 
+                                xmlns="http://www.w3.org/2000/svg" 
+                                width="24" 
+                                height="24" 
+                                viewBox="0 0 24 24" 
+                                fill="none" 
+                                stroke="currentColor" 
+                                strokeWidth="2" 
+                                strokeLinecap="round" 
+                                strokeLinejoin="round">
+                                <polyline points="9 18 15 12 9 6"></polyline>
+                            </svg>
+                        </Link> 
+                    </div>
+                </div>
                 {this.props.polls.size > 0 ? this.props.polls.map(poll => (
-                    <Link
-                        to={`/polls/${poll.get('id')}`}
-                        key={poll.get('id')}>
-                            <div className='poll'>
-                                {poll.get('question')}
-                            </div>
-                    </Link>
+                    <div className='poll' key={poll.get('id')}>                     
+                        <Link to={`/polls/${poll.get('id')}`}>
+                            {poll.get('question')}
+                        </Link>
+                        <p>Asked {moment(poll.get('createdAt')).fromNow()}</p>
+                        <hr/>
+                    </div>
                 )) : (
                     <div className='poll'>No Polls</div>
                 )}
-                <div className='link-container'>
-                    <div className='back'>
-                        &#9666; <Link to="/">Back</Link>
-                    </div>
-                    <div className='add-a-poll'>
-                        <Link to="/new">Add a Poll</Link> &#9656; 
-                    </div>
-                </div>
 			</div>
 		);
 	}
