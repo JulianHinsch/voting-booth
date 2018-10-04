0d1ae27643e4749814d75b79be28ec8d1add9015
Julian Hinsch <jhinsch799@gmail.com>
2018-10-04 10:38:51 -0400

    cleaning up

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-//import Hamburger from './Hamburger.js';
 
-export default class Header extends Component {
-	render() {
-        return (
-            <header>
-                <Link to="/" className="logo-container">
-                    <img 
-                        className='logo'
-                        src={require('../assets/logo.png')} 
-                        alt="Voting Booth" />
-                    <img 
-                        className='text-logo'
-                        src={require('../assets/textlogo.png')} 
-                        alt="Voting Booth"/>
-                </Link>
-            </header>
-        )
-    }
+const Header = () => {
+    return (
+        <header>
+            <Link to="/" className="logo-container">
+                <img 
+                    className='logo'
+                    src={require('../assets/logo.png')} 
+                    alt="Voting Booth" />
+                <img 
+                    className='text-logo'
+                    src={require('../assets/textlogo.png')} 
+                    alt="Voting Booth"/>
+            </Link>
+        </header>
+    )
 }
+
+export default Header;
--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const NotFound = () => (
-    <div className='not-found content'>
-        <h1>404</h1>
-        <p>Page not found.</p>
-    </div>
-)
+const NotFound = () => {
+    return (
+        <div className='not-found content'>
+            <h1>404</h1>
+            <p>Page not found.</p>
+        </div>
+    )
+}
 
 export default NotFound;
